067ec74690e5a34766b489a6cb989c1316ebb1e1
Gazar <kolajokimzee2020@gmailcom>
2025-08-22 16:21:32 +0100

    tenth commit

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from  django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.views import RegisterView, UserProfileView
+
+urlpatterns = [
+    path("signup/", RegisterView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("profile/", UserProfileView.as_view(), name="profile"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import  get_user_model
 
-# Create your views here.
+from rest_framework import generics, permissions, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+class UserProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        if hasattr(request.user, "auth_token"):
+            request.user.auth_token.delete()
+        return Response(
+            {"message": "You have been logged out successfully."},
+            status=status.HTTP_200_OK
+        )
+    def get_object(self):
+        return self.request.user
