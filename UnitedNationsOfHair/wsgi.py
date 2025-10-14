46f9f5889766a518e717f1ad1987c1bad226d5f4
gazar <kolajokimzee2020@gmail.com>
2025-10-14 23:26:56 +0100

    manage

--- UnitedNationsOfHair/wsgi.py
+++ UnitedNationsOfHair/wsgi.py
@@ -7,16 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
 
 from UnitedNationsOfHair import settings
 
+import os
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UnitedNationsOfHair.settings')
 
-
 application = get_wsgi_application()
+
 
 if settings.DEBUG:
     from users.models import User
