4e618fe797827c789c58d91fadf5ab5b188a7303
gazar <kolajokimzee2020@gmail.com>
2025-10-14 23:25:23 +0100

    manage

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+
+
 def main():
     print("Hello from unitednationsofhaire!")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UnitedNationsOfHair.settings')
 
 
 if __name__ == "__main__":
