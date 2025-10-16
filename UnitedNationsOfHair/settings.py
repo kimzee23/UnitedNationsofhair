9af824c77581d98adcdd0c0e564cd8ed7372fd11
gazar <kolajokimzee2020@gmail.com>
2025-10-16 07:54:19 +0100

    Add render domain to ALLOWED_HOSTS

--- UnitedNationsOfHair/settings.py
+++ UnitedNationsOfHair/settings.py
@@ -19,7 +19,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'united-nations-of-hair-backend.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
+
 
 AUTH_USER_MODEL = "users.User"
 
